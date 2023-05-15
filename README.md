80eaf19c305a8d8c08a02d54921949b5d1a6d13b
mojbrat <101670287+mojbrat@users.noreply.github.com>
2023-05-15 10:03:53 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Spotify Poster Generator
+
+https://poster-generator.up.railway.app/
+
+Does what it says on the tin.
+
+Enter a (valid) Spotify album URL, receive a poster!
+
+### Issues/Limitations (to be fixed maybe):
+- [ ] Special characters not supported
+- [ ] No scaling for too many characters
+- [ ] Better font
+- [ ] No checking for validity of URL
+- [ ] Choose poster size
+  
+---
+
+You'll need a .env file with the following values:
+- SPOTIFY_SECRET = `your spotify secret code`
+- SPOTIFY_ID = `your spotify client id`
+- FLASK_SECRET = `your flask secret code`
+
+---
+The website probably wont be up all the time, just email me if you need one.
---
+++ poster_generator.py
@@ -0,0 +1,146 @@
+"""
+     This file is part of poster-gen.
+
+    poster-gen is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    poster-gen is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with poster-gen. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+import numpy as np
+from PIL import Image
+import cv2
+from skimage import io
+from utils import *
+
+
+def generator(album, resolution):
+
+    data = spotify_data_pull(album)
+
+    spacing = 100
+
+    y_position = 2*spacing
+
+    # define poster size
+    poster = np.ones(resolution, np.uint8)
+    poster = poster*255
+
+    # album art
+    album_art = io.imread(data['album_art'])
+    album_art = cv2.resize(album_art, (resolution[1]-2*spacing, resolution[1]-2*spacing), cv2.INTER_AREA)
+    album_art = cv2.cvtColor(album_art, cv2.COLOR_RGB2BGR)
+
+    mask = np.zeros((album_art.shape[0], album_art.shape[1]), np.uint8)
+
+    mask = rounded_rectangle(mask, (0,0), (album_art.shape[0], album_art.shape[1]), 0.1, color=(255,255,255), thickness=-1)
+
+    art_inv = cv2.bitwise_not(album_art)
+
+    album_art = cv2.bitwise_not(cv2.bitwise_and(art_inv, art_inv, mask=mask))
+
+    x_offset = y_offset = spacing
+
+    poster[y_offset:y_offset+album_art.shape[0], x_offset:x_offset+album_art.shape[1]] = album_art
+
+    y_position += album_art.shape[0] + 2*spacing
+
+    # album artist
+    font_scale = 0
+    for i in range(2*spacing, 50, -5):
+        i = i/100
+        textsize = cv2.getTextSize(data['album_artist'], cv2.FONT_HERSHEY_TRIPLEX, i*10, 15)
+      
+        if textsize[0][0] <= (resolution[1]-2*spacing):
+            font_scale = i*10
+
+    cv2.putText(poster, data['album_artist'], (spacing, y_position), cv2.FONT_HERSHEY_TRIPLEX, font_scale, (0,0,0), 15)
+
+    y_position += 2*spacing
+
+    # album name
+    font_scale = 0
+    for i in range(2*spacing, 50, -5):
+        i = i/100
+        textsize = cv2.getTextSize(data['album_artist'], cv2.FONT_HERSHEY_TRIPLEX, i*10, 5)
+        
+        if textsize[0][0] <= (resolution[1]-2*spacing):
+            font_scale = i*10
+
+    cv2.putText(poster, data['album_name'], (spacing, y_position), cv2.FONT_HERSHEY_PLAIN, font_scale, (0,0,0), 5)
+
+    # playtime
+    playtime_size = cv2.getTextSize(data['playtime'], cv2.FONT_HERSHEY_PLAIN, 3.5, 5)[0][0]
+    cv2.putText(poster, data['playtime'], (resolution[1]-spacing-playtime_size, y_position), cv2.FONT_HERSHEY_PLAIN, 3.5, (0,0,0), 5)
+
+    y_position += spacing
+
+    # color palette
+
+    palette = dominant_colors(album_art)
+
+    num_colors = 10
+    color_palette = np.ones((5, num_colors*100, 3), np.uint8)
+    for i in range(num_colors):
+        section = 100*(i+1)
+        cv2.rectangle(color_palette, (section-100,0), (section, 100), palette[i], -1)
+
+    color_palette = cv2.resize(color_palette, (resolution[1]-2*spacing, 50), cv2.INTER_AREA)
+
+    poster[y_position:y_position+color_palette.shape[0], x_offset:x_offset+color_palette.shape[1]] = color_palette
+
+    y_position += 2*spacing
+
+    # tracks
+
+    track_line = ""
+    for track in data['tracks']:
+        if cv2.getTextSize(track_line, cv2.FONT_HERSHEY_PLAIN, 5, 10)[0][0] < resolution[1]-spacing:
+            track_line = track_line + track + " | "
+            
+        if cv2.getTextSize(track_line, cv2.FONT_HERSHEY_PLAIN, 5, 10)[0][0] >= resolution[1]-spacing:
+            track_line = track_line[:len(track_line) - len(track + " | ")]
+            cv2.putText(poster, track_line, (spacing, y_position), cv2.FONT_HERSHEY_PLAIN, 5, (0,0,0), 5)
+            track_line = track + " | "
+            y_position += cv2.getTextSize(track_line, cv2.FONT_HERSHEY_PLAIN, 5, 10)[0][1] + 50
+    cv2.putText(poster, track_line, (spacing, y_position), cv2.FONT_HERSHEY_PLAIN, 5, (0,0,0), 5)
+
+    #spotify logo
+    scale = 0.04
+    creditslogo = cv2.imread("spotifylogo.jpg")
+    creditslogo = cv2.resize(creditslogo, (int(resolution[0]*scale), int(resolution[0]*scale)))
+    poster[resolution[0]-spacing-creditslogo.shape[0]:resolution[0]-spacing, resolution[1]-spacing-creditslogo.shape[1]:resolution[1]-spacing] = creditslogo
+    
+    # record label
+    cv2.putText(poster, data['record'], (spacing, resolution[0]-163), cv2.FONT_HERSHEY_PLAIN, 3.5, (0,0,0), 5)
+
+    # release date
+    cv2.putText(poster, data['release_date'], (spacing, resolution[0]-spacing), cv2.FONT_HERSHEY_PLAIN, 3.5, (0,0,0), 5)
+
+    poster = cv2.cvtColor(poster, cv2.COLOR_BGR2RGB)
+
+    return(poster, data['album_name'])
+
+if __name__ == '__main__':
+
+    album = input("Enter Spotify Album link: ")
+    if album.find('https://open.spotify.com/album/') == -1:
+        print("Enter valid Spotify album link.")
+        exit(1)
+
+    resolution = input("Enter height, width in pixels: ")
+    if resolution == '':
+        resolution = (5100, 3300, 3)
+    else:
+        resolution = list(map(int, resolution.strip().split(',')))
+        resolution.append(3)
+
+    poster, album_name = generator(album, resolution)
+
+    poster = cv2.cvtColor(poster, cv2.COLOR_RGB2BGR)
+
+    cv2.imwrite("{}_poster.jpg".format(album_name), poster)
+
+    Image.open("{}_poster.jpg".format(album_name)).show()
+ 
